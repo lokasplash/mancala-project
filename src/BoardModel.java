b8a9d6929e658e5addabf02d9bf57f43bf263163
Andrew Jong <andrewjong87@gmail.com>
2017-04-07 13:49:26 -0700

    Added board model class with array to hold pieces

---
+++ src/BoardModel.java
@@ -0,0 +1,14 @@
+/**
+ * Created by andrew on 4/7/17.
+ */
+public class BoardModel {
+	private int[][] pits = new int[2][6];
+
+	public int[][] getPits() {
+		return pits;
+	}
+
+	public void setPits(int[][] pits) {
+		this.pits = pits;
+	}
+}

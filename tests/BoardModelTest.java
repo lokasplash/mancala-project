94bb3c612d22e9d44052c11808a311c2fc976c87
lokasplash <lokasplash@users.noreply.github.com>
2017-04-20 18:47:30 -0700

    Revert "Revert "Controller pit playerground edit""

--- tests/BoardModelTest.java
+++ tests/BoardModelTest.java
@@ -1,14 +1,8 @@
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-//import static org.junit.jupiter.api.Assertions.*;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Tests for the BoardModel class.

51a3528ea47679cfbaf7a9b03fa7cb1972f2e526
Andrew Jong <andrewjong87@gmail.com>
2017-04-22 14:59:42 -0700

    Created placeholder classes for view to call

---
+++ src/PlayerTurnPanel.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class PlayerTurnPanel extends JPanel {
+	boolean playerTurn;
+	public void setPlayerTurn(boolean playerTurn) {
+		this.playerTurn = playerTurn;
+	}
+}

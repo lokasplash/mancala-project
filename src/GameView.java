1826f4382f17866d2291e81cbe90582cdaf7a8cf
Vincent D <lokajii@yahoo.com>
2017-05-05 22:09:25 -0700

    added mancalaPanel placeholder

--- src/GameView.java
+++ src/GameView.java
@@ -18,18 +18,42 @@ public class GameView {
 		PlayerTurnPanel playerTurnPanel = new PlayerTurnPanel(gameModel);
 		frame.add(playerTurnPanel, BorderLayout.NORTH);
 
-//		 Board Panel
+		// Board Panel
 		BoardPanel boardPanel = new BoardPanel();
+		
+		/* Temporary placeholder */
+			// left side mancala
+				JPanel mancalaLeft = new JPanel();
+				mancalaLeft.setPreferredSize(new Dimension(200,400));
+				mancalaLeft.setBackground(java.awt.Color.GREEN);
+				boardPanel.left.add(mancalaLeft);
+				
+				// right side mancala
+				JPanel mancalaRight = new JPanel();
+				mancalaRight.setPreferredSize(new Dimension(200,400));
+				mancalaRight.setBackground(java.awt.Color.GREEN);
+				boardPanel.right.add(mancalaRight);
+				
+				
+				// board center
+				JPanel boardCenter = new JPanel();
+				boardCenter.setPreferredSize(new Dimension(600,400));
+				boardCenter.setBackground(java.awt.Color.PINK);
+				boardPanel.center.add(boardCenter);
+		/* End temp placeholder */
+//
+//		GridPanel gridPanel = new GridPanel();
+//		MancalaPanel mancalaPanelP1 = new MancalaPanel();
+//		
+//		
+//		MancalaPanel mancalaPanelP2 = new MancalaPanel();
+//		boardPanel.left = mancalaPanelP2;
+//		boardPanel.center = gridPanel;
+//		boardPanel.right = mancalaPanelP1;
 
-		GridPanel gridPanel = new GridPanel();
-		MancalaPanel mancalaPanelP1 = new MancalaPanel();
-		MancalaPanel mancalaPanelP2 = new MancalaPanel();
-		boardPanel.left = mancalaPanelP2;
-		boardPanel.center = gridPanel;
-		boardPanel.right = mancalaPanelP1;
 
 
-//		frame.add(boardPanel, BorderLayout.CENTER);
+		frame.add(boardPanel, BorderLayout.CENTER);
 
 		// Undo/redo controls
 		UndoRedoPanel undoRedoPanel = new UndoRedoPanel();
@@ -45,15 +69,15 @@ public class GameView {
 			// set the correct turn
 			playerTurnPanel.setPlayerTurn(boardData.PLAYER_1_TURN);
 			// set the board view
-			mancalaPanelP1.setMancala(boardData.PLAYER_1_MANCALA);
+//			mancalaPanelP1.setMancala(boardData.PLAYER_1_MANCALA);
 
 //			PitPanel[] p1Pits = (PitPanel[]) Arrays.stream(boardData.PLAYER_1_PITS).mapToObj(PitPanel::new).toArray();
 //			gridPanel.setP1Pits(p1Pits);
 //			PitPanel[] p2Pits = (PitPanel[]) Arrays.stream(boardData.PLAYER_1_PITS).mapToObj(PitPanel::new).toArray();
 //			gridPanel.setP2Pits(p2Pits);
 			// set the undo/redo buttons as available or not based on undo history
-//			undoRedoPanel.setUndoButton(gameModel.canUndo());
-//			undoRedoPanel.setRedoPanel(gameModel.canRedo());
+			undoRedoPanel.setUndoButton(gameModel.canUndo());
+			undoRedoPanel.setRedoButton(gameModel.canRedo());
 
 		});
 	}
